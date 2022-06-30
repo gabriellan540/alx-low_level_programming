f6082c11f0a29e5f87f2968a371240daed742e9a
gabriellan540 <gabriellan540@gmail.com>
2022-06-30 12:41:15 -0700

    puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,16 @@
-#include <stdio.h>
-/**
-*this program prints "Programming is like building a multilingual puzzle and returns 0
-*/
+#include <stdio.h>                                                                                                                      
+
+/**                                                                                                                                     
+
+*this program prints "Programming is like building a multilingual puzzle and returns 0                                                  
+
+*/ 
+int main(void)
+
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
+}
