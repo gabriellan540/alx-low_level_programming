80d3677775f59562902bb96d02e21ac19415f0f4
gabriellan540 <gabriellan540@gmail.com>
2022-06-30 13:27:06 -0700

    puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
-/*
-* this program prints "Programming is like building a multilingual puzzle
+
+/**
+* main - print the string in the put function
+* Description: using the main function
 * Return: 0
 */
 int main(void)
