02c37b39bbc3bcbca5e34767a170f89e4bc09dc0
gabriellan540 <gabriellan540@gmail.com>
2022-07-01 15:28:31 -0700

    combination

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - Entry point
+* Return: 0
+*/
+
+int main(void)
+{
+int i, j;
+
+for (i = '0'; i <= '9'; i++)
+{
+for (j = i + 1; j <= '9'; j++)
+{
+if (j != i)
+{
+putchar(i);
+putchar(j);
+if (i == '8' && j == '9')
+continue;
+
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+
+return (0);
+}
