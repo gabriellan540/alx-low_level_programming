05bb3f63dabadcd31f27916942042ecd452c8296
gabriellan540 <gabriellan540@gmail.com>
2022-07-01 14:19:03 -0700

    lower and upper

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - Entry point
+* Return: Always 0
+*/
+int main(void)
+{
+char alphabet;
+
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+putchar(alphabet);
+for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
+putchar(alphabet);
+putchar('\n');
+
+return (0)
+}
