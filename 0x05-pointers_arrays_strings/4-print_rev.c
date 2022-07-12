3c58ce21898e2f6ad2b124dcf77532f490947188
gabriellan540 <gabriellan540@gmail.com>
2022-07-12 12:28:03 -0700

    strcpy

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* rev_string - reverse string
+* @s: string
+* Return: void
+*/
+
+void rev_string(char *s)
+{
+int f, r;
+char c;
+
+for (f = '\0'; s[f] != 0; f++)
+{
+}
+r = 0;
+
+for (f = f - 1; r < f; r++)
+{
+c = s[f];
+s[f] = s[r];
+s[r] = c
