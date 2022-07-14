535d6b9658c20000d28282b640273e1e1826a9e4
Gabriella <gabriellan540@gmail.com>
2022-07-14 23:27:54 +0300

    Update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -3,15 +3,14 @@
 /**
 * leet - encodes a string into 1337
 * @s: input string
- * Return: the pointer to dest
+* Return: the pointer to dest
 */
 
 char *leet(char *s)
 {
 int num = 0, a;
 int lower[] = {97, 101, 111, 116, 108};
-int upper[] = 
-{65, 69, 79, 84, 76};
+int upper[] = {65, 69, 79, 84, 76};
 int numbers[] = {52, 51, 48, 55, 49};
 
 while (*(s + num) != '\0')
@@ -19,11 +18,12 @@ while (*(s + num) != '\0')
 
 for (a = 0; a < 5; a++)
 {
-if (*(s + numt) == lower[a] || *(s + numt) == upper[a])
+if (*(s + num) == lower[a] || *(s + num) == upper[a])
 {
 *(s + num) = numbers[a];
 break;
 }
+  
 num++;
 }
 return (s);
