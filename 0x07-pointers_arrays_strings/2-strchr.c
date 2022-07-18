a21ac7a3d86fcb4cab9fb86016eb614408d178ee
gabriellan540 <gabriellan540@gmail.com>
2022-07-18 12:40:29 -0700

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *  _strchr - finds c
+ *  @s: pointer to char
+ *  @c: char params to be found
+ *  Return: *S
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+
+return (0);
+}
+
