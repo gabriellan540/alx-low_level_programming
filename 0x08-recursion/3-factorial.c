656ca61f6c98a67f2f437c3fa70363f41403ec18
Gabriella <gabriellan540@gmail.com>
2022-07-20 22:55:18 +0300

    factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -8,16 +8,16 @@
  
 int factorial(int n)
 {
-if (n < 0)
-  {
-    return (-1);
-  }
-  else
-  {
-    if (n == 0)
-      return (1);
-    else if (n == 1)
-      return (1);
-    return (factorial(n - 1) * n);
-  }
+ if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		if (n == 0)
+			return (1);
+		else if (n == 1)
+			return (1);
+		return (factorial(n - 1) * n);
+	}
 }
